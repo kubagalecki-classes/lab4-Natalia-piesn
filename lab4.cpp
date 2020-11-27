8d53a9a075d155f0e7e457c9163eee2923c085c3
Nataliapiesn <01123786@pw.edu.pl>
2020-11-27 21:09:32 +0000

    Task 1 completed.

--- lab4.cpp
+++ lab4.cpp
@@ -1,6 +1,28 @@
 #include <iostream>
+using namespace std;
+
+template < typename T >
+
+class Couple
+{
+public:
+    Couple(T an, T bn)
+    {
+        a = an;
+        b = bn;
+    }
+    void set_a(T param) { a = param; }
+    void set_b(T param) { b = param; }
+    T    get_a() { return a; }
+    T    get_b() { return b; }
+
+private:
+    T a;
+    T b;
+};
 
 int main()
 {
-    puts("Hello, World!");
+    Couple p1 = Couple{'a', 'b'};
+    cout << p1.get_a() << " " << p1.get_b() << endl;
 }
