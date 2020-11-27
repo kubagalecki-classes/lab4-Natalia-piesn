986a932fc41cd99b7a896b192ef0315477e1638e
Nataliapiesn <01123786@pw.edu.pl>
2020-11-27 23:53:40 +0000

    sumCouple function template added. Task 9 completed.

--- lab4.cpp
+++ lab4.cpp
@@ -51,35 +51,46 @@ struct S< double >
 template < typename T >
 T product(T tab[], int n)
 {
-    T prod = 0;
-    for (int i = 0; i < n; i++) {
+    T prod = tab[0];
+    for (int i = 1; i < n; i++) {
         prod = prod * tab[i];
     }
+    return prod;
 }
 
+template < typename T >
+T sumCouples(Couple< T > p)
+{
+    return p.sum();
+}
 int main()
 {
-    S< char >   i;
-    S< double > d;
-    i.print();
-    d.print();
-    
+    Couple< double > p(1.3, 2);
+    cout << sumCouples(p) << endl;
     /*
-    Couple< double > p1(1, 2);
-    Couple< double > p2(3.2, 5.4);
-    Couple< double > p3(1.1, 2.3);
+      S< char >   i;
+      S< double > d;
+      i.print();
+      d.print();
+      double tab[]    = {1.1, 2.2, 3.3};
+      double silnia_3 = product(tab, 3);
+      cout << silnia_3;
 
-    cout << p1.get_a() << " " << p1.get_b() << endl;
-    cout << p1.sum() << endl;
-    CoupleTable< double, 4 > tab;
-    cout << sizeof(tab.table) << endl;
-    tab.set_el(0, p1);
-    tab.set_el(1, p2);
-    tab.set_el(2, p3);
-    tab.set_el(3, p2);
-    cout << tab.get_el(0).sum() << endl;
-    cout << tab.get_el(1).sum() << endl;
-    cout << tab.get_el(2).sum() << endl;
-    cout << tab.get_el(3).sum() << endl;
-    */
+      Couple< double > p1(1, 2);
+      Couple< double > p2(3.2, 5.4);
+      Couple< double > p3(1.1, 2.3);
+
+      cout << p1.get_a() << " " << p1.get_b() << endl;
+      cout << p1.sum() << endl;
+      CoupleTable< double, 4 > tab;
+      cout << sizeof(tab.table) << endl;
+      tab.set_el(0, p1);
+      tab.set_el(1, p2);
+      tab.set_el(2, p3);
+      tab.set_el(3, p2);
+      cout << tab.get_el(0).sum() << endl;
+      cout << tab.get_el(1).sum() << endl;
+      cout << tab.get_el(2).sum() << endl;
+      cout << tab.get_el(3).sum() << endl;
+      */
 }
